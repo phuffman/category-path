29cef6251166264fb49592c6d8b8e3029382661f
phuffman <paul.huffman@ipfolio.com>
2020-10-10 12:27:47 -0700

    Early changes

--- category-path.go
+++ category-path.go
@@ -13,35 +13,35 @@ import (
 func main() {
 
 	if len(os.Args) != 3 {
-		fmt.Println("Use the category-path file-in file-out")
+		fmt.Println("Try: category-path in.csv out.txt")
 		return
 	}
 
 	// Open the file (f)
-	filename := os.Args[1]
-	f, err := os.Open(filename)
+	fin := os.Args[1]
+	f, err := os.Open(fin)
 	checkError("Cannot open file", err)
 	defer f.Close()
 
-	// Create map to hold categories (m)
+	// Create empty map (m) to hold categories
 	m := make(map[string]string)
 
-	// Create reader (r)
+	// Create reader (r) to read csv
 	r := csv.NewReader(f)
 
 	// Read csv to variable (rows)
 	rows, err := r.ReadAll()
 	checkError("Can't read file", err)
 
-	// Add categories to map (m)
+	// Iterate csv to add categories to map (m)
 	for _, row := range rows {
 		m[row[1]] = row[2]
 	}
 
-	// Create slice (path) to hold all paths
+	// Create empty string slice (path) to hold all paths
 	paths := []string{}
 
-	// Iterate csv (rows)
+	// Iterate category csv (rows) to create a full path for each. Add all to string slice (paths).
 	for _, row := range rows {
 
 		// Create variables to hold current csv row
@@ -52,41 +52,41 @@ func main() {
 		// Create slice (path) to hold current path
 		path := []string{}
 
-		// 1) Add category type to 'path'
+		// 1) First, add category type
 		path = append(path, categorytype)
 
-		// 2) Add parent to 'path', then loop adding parent of parent, etc. to 'path'
+		// 2) Then, add parent, parent of parent, etc.
 		for parent != "" {
 			path = append(path, parent)
 			parent = m[parent]
 		}
-		// 3) Finally, add category to 'path'
+		// 3) Finally, add category
 		path = append(path, category)
 
-		// Add 'path' to 'paths'
+		// Add current 'path' to 'paths'
 		paths = append(paths, strings.Join(path, " -> "))
 	}
 
 	// Sort paths
 	sort.Strings(paths)
 
+	// Convert slice to string (output)
 	output := strings.Join(paths, "\n")
 
+	// Print 'output' to screen
 	println(output)
 
-	/*
-		for _, line := range paths {
-			println(line)
-		}
-	*/
-
-	file, err := os.Create("./out.csv")
+	// Create output file
+	fout := os.Args[2]
+	file, err := os.Create(fout)
 	checkError("Can't create file", err)
 	defer file.Close()
 
+	// Write string to output file
 	ln, err := io.WriteString(file, output)
 	checkError("Can't write file", err)
 
+	// ln (above) is required, and it needs to be used somewhere, so I'm showing it here ...
 	fmt.Printf("String length: %v"+"\n", ln)
 }
 
