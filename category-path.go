b5b5f9400ec6fbe8a39833bfbbef660bd52856e1
phuffman <paul.huffman@ipfolio.com>
2020-10-11 00:16:07 -0700

    Changes

--- category-path.go
+++ category-path.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
-	"io"
 	"log"
 	"os"
 	"sort"
@@ -12,30 +11,60 @@ import (
 
 func main() {
 
-	if len(os.Args) != 3 {
-		fmt.Println("Try: category-path in.csv out.txt")
+	// Check command line for 'category-path in.csv out.txt'
+	if len(os.Args) != 2 {
+		fmt.Println("Try: category-path in.csv")
 		return
 	}
 
 	// Open the file (f)
-	argIn := os.Args[1]
-	f, err := os.Open(argIn)
+	f, err := os.Open(os.Args[1])
 	checkError("Cannot open file", err)
 	defer f.Close()
+
+	// Create a reader (r)
+	r := csv.NewReader(f)
+
+	// Read the first row (header)
+	header, err := r.Read()
+	checkError("Can't open header", err)
+
+	type Columns struct {
+		categorytype, parent, category int
+	}
+	var cols Columns
+
+	fmt.Printf("cateory = %T\n", cols.category)
+
+	i := 0
+	for _, column := range header {
+		switch {
+		case strings.Contains(column, "Category Type"):
+			cols.categorytype = i
+		case strings.Contains(column, "Parent Category"):
+			cols.parent = i
+		case strings.Contains(column, "Category Name"):
+			cols.category = i
+		}
+		i++
+	}
+
+	// Verify Category and Parent Cateory columns exist
+	if cols.category == 0 || cols.parent == 0 {
+		fmt.Println("I need columns called 'Category Name' and 'Parent Category'.")
+		return
+	}
 
 	// Create empty map (m) to hold categories
 	m := make(map[string]string)
 
-	// Create reader (r) to read csv
-	r := csv.NewReader(f)
-
-	// Read csv to variable (rows)
+	// Read remaining rows to variable (rows)
 	rows, err := r.ReadAll()
 	checkError("Can't read file", err)
 
 	// Iterate csv to add categories to map (m)
 	for _, row := range rows {
-		m[row[1]] = row[2]
+		m[row[cols.category]] = row[cols.parent]
 	}
 
 	// Create empty string slice (path) to hold all paths
@@ -44,10 +73,9 @@ func main() {
 	// Iterate category csv (rows) to create a full path for each. Add all to string slice (paths).
 	for _, row := range rows {
 
-		// Create variables to hold current csv row
-		categorytype := row[0]
-		parent := row[1]
-		category := row[2]
+		// Create variables to hold parent and category from current row
+		parent := row[2]
+		category := row[1]
 
 		// Create slice (path) to hold current path
 		path := []string{}
@@ -60,8 +88,12 @@ func main() {
 			path = append([]string{parent}, path...)
 			parent = m[parent]
 		}
-		// 3) Finally, prepend category type
-		path = append([]string{categorytype}, path...)
+
+		// 3) Finally, if exists, prepend category type
+		if cols.categorytype != 0 {
+			categorytype := row[cols.categorytype]
+			path = append([]string{categorytype}, path...)
+		}
 
 		// Add current 'path' to 'paths'
 		paths = append(paths, strings.Join(path, " > "))
@@ -71,28 +103,24 @@ func main() {
 	sort.Strings(paths)
 
 	// Convert slice to string (output)
-	output := strings.Join(paths, "\n")
+	// output := strings.Join(paths, "\n")
+
+	// Print 'output' to screen
+	// println(output)
 
 	/*
-		// Print 'output' to screen
-		println(output)
+		// Create output file
+		file, err := os.Create("out.csv")
+		checkError("Can't create file", err)
+		defer file.Close()
+
+		// Write string to output file
+		ln, err := io.WriteString(file, output)
+		checkError("Can't write file", err)
+
+		// ln (above) is required, and it needs to be used somewhere, so I'm showing it here ...
+		fmt.Printf("String length: %v"+"\n", ln)
 	*/
-
-	// Create output file
-	argOut := os.Args[2]
-	file, err := os.Create(argOut)
-	checkError("Can't create file", err)
-	defer file.Close()
-
-	// Write string to output file
-	ln, err := io.WriteString(file, output)
-	checkError("Can't write file", err)
-
-	// ln (above) is required, and it needs to be used somewhere, so I'm showing it here ...
-	fmt.Printf("String length: %v"+"\n", ln)
-
-	println(argOut)
-
 }
 
 func checkError(message string, err error) {
