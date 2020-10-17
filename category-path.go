5e9cf1a51e8711705aedb208575590d46deb4a83
phuffman <paul.huffman@ipfolio.com>
2020-10-17 10:39:19 -0700

    Learning

--- category-path.go
+++ category-path.go
@@ -12,57 +12,74 @@ import (
 
 func main() {
 
-	// Check command line for 'category-path in.csv'
+	// Verify input has 2 args
 	if len(os.Args) != 2 {
 		fmt.Println("Try: category-path in.csv")
 		return
 	}
 
-	// Open the file (f)
+	// Open file (f)
 	f, err := os.Open(os.Args[1])
-	checkError("Cannot open file", err)
+	if err != nil {
+		log.Fatal("Can't open file", err)
+	}
 	defer f.Close()
 
 	// Create a reader (r)
 	r := csv.NewReader(f)
 
-	// Read the first row (header)
+	// Get header row (header)
 	header, err := r.Read()
-	checkError("Can't open header", err)
-
-	type Columns struct {
-		categorytype, parent, category int
+	if err != nil {
+		log.Fatal("Can't open header", err)
 	}
 
-	cols := Columns{0, 0, 100}
+	// Get column numbers of key fields
 
+	// Create struct (Columns)
+	type Columns struct {
+		categorytype int
+		parent       int
+		category     int
+	}
+
+	// Create Columns variable (cols)
+	cols := Columns{
+		categorytype: -1,
+		parent:       -1,
+		category:     -1,
+	}
+
+	// Get column number of name, parent and type columns (-1 if not found)
 	i := 0
 	for _, column := range header {
 		switch {
-		case strings.Contains(column, "Category Type"):
+		case strings.Contains(column, "Type"):
 			cols.categorytype = i
-		case strings.Contains(column, "Parent Category"):
+		case strings.Contains(column, "Parent"):
 			cols.parent = i
-		case strings.Contains(column, "Category Name"):
+		case strings.Contains(column, "Name"):
 			cols.category = i
 		}
 		i++
 	}
 
-	// Verify Category and Parent Cateory columns exist
-	if cols.category == 0 || cols.parent == 0 {
-		fmt.Println("I need columns called 'Category Name' and 'Parent Category'.")
+	// Verify category and parent cateory exist
+	if cols.category == -1 || cols.parent == -1 {
+		fmt.Println("You need columns called 'Category Name' and 'Parent Category'.")
 		return
 	}
 
 	// Create empty map (m) to hold categories
 	m := make(map[string]string)
 
-	// Read remaining rows to variable (rows)
+	// Get remaining rows (rows)
 	rows, err := r.ReadAll()
-	checkError("Can't read file", err)
+	if err != nil {
+		log.Fatal("Can't read file", err)
+	}
 
-	// Iterate csv to add categories to map (m)
+	// Iterate csv, adding categories to map (m)
 	for _, row := range rows {
 		m[row[cols.category]] = row[cols.parent]
 	}
@@ -70,7 +87,8 @@ func main() {
 	// Create empty string slice (path) to hold all paths
 	paths := []string{}
 
-	// Iterate category csv (rows) to create a full path for each. Add all to string slice (paths).
+	// Iterate csv again to create a full path for each.
+	// Add each path to slice (paths).
 	for _, row := range rows {
 
 		// Create variables to hold parent and category from current row
@@ -89,8 +107,8 @@ func main() {
 			parent = m[parent]
 		}
 
-		// 3) Finally, if exists, prepend category type
-		if cols.categorytype != 100 {
+		// 3) Finally, prepend category type (if there is one)
+		if cols.categorytype != -1 {
 			categorytype := row[cols.categorytype]
 			path = append([]string{categorytype}, path...)
 		}
@@ -105,24 +123,19 @@ func main() {
 	// Convert slice to string (output)
 	output := strings.Join(paths, "\n")
 
-	// Print 'output' to screen
-	println(output)
+	// 'output' to screen
+	// println(output)
 
-	// Create output file
+	// Create output file (out.csv)
 	file, err := os.Create("out.csv")
-	checkError("Can't create file", err)
+	if err != nil {
+		log.Fatal("Can't create file", err)
+	}
 	defer file.Close()
 
-	// Write string to output file
-	ln, err := io.WriteString(file, output)
-	checkError("Can't write file", err)
-
-	// ln (above) is required, and it needs to be used somewhere, so I'm showing it here ...
-	fmt.Printf("String length: %v"+"\n", ln)
-}
-
-func checkError(message string, err error) {
+	// Write to output file
+	_, err = io.WriteString(file, output)
 	if err != nil {
-		log.Fatal(message, err)
+		log.Fatal("Can't write file", err)
 	}
 }
